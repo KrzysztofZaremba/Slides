1f22b058ce6b075c202194ee8c3366800a21f065
KrzysztofZaremba <kzysiek92@gmail.com>
2023-09-27 20:24:04 -0600

    update

--- C4/C4_b/auto_cor.R
+++ C4/C4_b/auto_cor.R
@@ -1,3 +1,4 @@
+
 # Set seed for reproducibility
 set.seed(123)
 
@@ -10,10 +11,17 @@ scatterplot_list <- list()
 
 # Initialize a list to store autocorrelation coefficients
 autocorrelation_coefficients <- list()
-ggplot_list <-vector("list", length = 5)
-lag=2
+ggplot_list <- vector("list", length = 5)
+
 # Create scatterplots for residual vs. lagged residuals
-for (lag in 1:5) {  # Lag from 1 to 5
+# Generate data with correlated residuals
+correlation_values <- c(0.8, 0.6, 0.4, 0.2, 0)  # Decreasing correlation values
+scatterplot_list <- list()
+
+# Initialize a list to store autocorrelation coefficients
+autocorrelation_coefficients <- list()
+
+autocor = function (lag) {  # Lag from 1 to 5
   # Create correlated residuals with decreasing correlation
   correlation <- correlation_values[lag]
   cov_matrix <- matrix(c(1, correlation, correlation, 1), ncol = 2)
@@ -23,24 +31,24 @@ for (lag in 1:5) {  # Lag from 1 to 5
   data1 <- data.frame(Residual_i = correlated_residuals[, 1])
   data1[[paste0("Residual_i-", lag)]] <- correlated_residuals[, 2]
   
-  # Create a scatter plot for residual vs. lagged residuals
-  message(lag)
-  
-  ggplot_list[[lag]]<-local({
-  lag<-lag
-   gg <- ggplot(data=data1, aes(x = Residual_i, y = data1[[paste0("Residual_i-", lag)]])) +
-    geom_point(color = "blue") +
-    labs(x = "Residual", y = paste0("Residual_i-", lag)) +
-    theme_minimal() +
-    ggtitle(paste("Lag:", lag))
-   print(gg)
-  })
-
-  # Calculate and store autocorrelation coefficient
   autocorr_coeff <- cor(data1$Residual_i, data1[[paste0("Residual_i-", lag)]])
-  autocorrelation_coefficients[[lag]] <- autocorr_coeff
 }
 
+# Create scatterplots and collect autocorrelation coefficients
+autocorrelation_coefficients=lapply(1:5, autocor)
 
-ggplot_list[[4]]
-grid.arrange(grobs = ggplot_list, ncol = 5)
+# Create a bar graph of autocorrelation coefficients
+bar_data <- data.frame(Lag = 1:5, Autocorrelation = unlist(autocorrelation_coefficients))
+bar_plot <- ggplot(bar_data, aes(x = factor(Lag), y = Autocorrelation)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Lag", y = "Autocorrelation") +
+  theme_xaringan()
+
+ggplot_list <- lapply(1:5, plot_data_column)
+# Arrange the scatterplots and bar graph side by side
+grid.arrange(
+  arrangeGrob(grobs = ggplot_list , ncol = 5),
+  bar_plot,
+  ncol = 1,
+  heights = c(3, 3)  # Adjust the height ratio as needed
+)
--- C4/C4_b/qqqplot.R
+++ C4/C4_b/qqqplot.R
@@ -49,6 +49,53 @@ determine the quantiles and put them ona  graph
 - build a table1
 
 
+library(ggplot2)
+library(gridExtra)
+
+# Set the seed for reproducibility
+set.seed(123)
+
+# Generate uniformly distributed residuals
+residuals <- runif(100, min = -1, max = 1)
+
+# Create a data frame with the residuals
+data_df <- data.frame(Residuals = residuals)
+
+# Create x values (original variable)
+data_df$x <- rnorm(100)
+
+# Create y values using a linear equation
+data_df$y <- 2 + 3 * data_df$x + residuals
+
+# Fit a linear regression model
+model <- lm(y ~ x, data_df)
+
+# Create a fitted regression plot
+regression_plot <- ggplot(data_df, aes(x = x, y = y)) +
+  geom_point(color = "black") +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(x = "X", y = "Y") +
+  theme_minimal()
+
+# Create a histogram of standardized residuals
+histogram_plot <- ggplot(data_df, aes(x = residuals)) +
+  geom_histogram(binwidth = 0.1, aes(y = after_stat(density)), fill = "green", color = "black", alpha = 0.7) +
+  geom_density(color = "blue") +
+  labs(x = "Standardized Residuals", y = "Density") +
+  theme_minimal()
+
+# Create a QQ plot
+qq_plot <- ggplot(data_df) +
+  geom_qq(aes(sample = residuals)) +
+  geom_abline(color = "red") +
+  coord_fixed() +
+  theme_minimal()+
+  xlim(-3,3)+
+  ylim(-3,3)
+
+# Arrange the three plots side by side
+grid.arrange(regression_plot, histogram_plot, qq_plot, ncol = 3)
+
 
 
 ####
