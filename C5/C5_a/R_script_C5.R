8c80ddfec224784274206ddfadc2ed553ca9dbee
KrzysztofZaremba <kzysiek92@gmail.com>
2023-11-22 19:50:39 -0600

    update

--- C5/C5_a/R_script_C5.R
+++ C5/C5_a/R_script_C5.R
@@ -1,4 +1,6 @@
 #### Loading the data 
+### If you want to replicate this, you can download the data here: http://www.dgis.salud.gob.mx/contenidos/basesdedatos/da_nacimientos_gobmx.html
+
 Nacimientos_2021 <- read.csv("C:/Users/kzysi/Dropbox/Birth_Mexico/Data/Nacimientos_2021.csv")
 Nacimientos_2021 = Nacimientos_2021[,c("PESO","EDADGESTACIONAL","EDAD","NUMEROEMBARAZOS","HIJOSNACIDOSMUERTOS",
                                        "TOTALCONSULTAS")]
@@ -27,6 +29,7 @@ summary(model2a)
 model3=lm(PESO~EDADGESTACIONAL+EDAD+I(EDAD^2)+NUMEROEMBARAZOS+HIJOSNACIDOSMUERTOS+TOTALCONSULTAS, data=Nacimientos_2021)
 summary(model3)
 
+
 #### Interpretations
 #1 Intercept
 #2 One more week of gestational age
@@ -39,6 +42,8 @@ summary(model3)
 model4=lm(scale(PESO)~scale(EDADGESTACIONAL)+scale(EDAD)+scale(I(EDAD^2))+scale(NUMEROEMBARAZOS)+scale(HIJOSNACIDOSMUERTOS)+scale(TOTALCONSULTAS), data=Nacimientos_2021)
 summary(model4)
 
+model4=lm(PESO~scale(EDADGESTACIONAL)+scale(EDAD)+scale(I(EDAD^2))+scale(NUMEROEMBARAZOS)+scale(HIJOSNACIDOSMUERTOS)+scale(TOTALCONSULTAS), data=Nacimientos_2021)
+summary(model4)
 ##what has the largest effect in terms of standard deviations? 1. gestational age, 2. numer of doctors visits
 
 #### Diagnostics
@@ -55,9 +60,21 @@ library(tidyverse)
 ggplot(data=Nacimientos_2021)+
   geom_point(aes(x=yhat, y=residuals), alpha=0.7)
 
+####
+model3=lm(PESO~EDADGESTACIONAL+EDAD+I(EDAD^2)+NUMEROEMBARAZOS+HIJOSNACIDOSMUERTOS+TOTALCONSULTAS+I(NUMEROEMBARAZOS^2), data=Nacimientos_2021)
+summary(model3)
+
+Nacimientos_2021$yhat=model3$fitted.values
+Nacimientos_2021$residuals=model3$residuals
+
+library(tidyverse)
+ggplot(data=Nacimientos_2021)+
+  geom_point(aes(x=yhat, y=residuals), alpha=0.7)
+
 ###We have some non-linearity
 model5=lm(PESO~EDADGESTACIONAL+I(EDADGESTACIONAL^2)+EDAD+I(EDAD^2)+NUMEROEMBARAZOS+HIJOSNACIDOSMUERTOS+TOTALCONSULTAS, data=Nacimientos_2021)
 summary(model5)
+
 
 Nacimientos_2021$yhat=model5$fitted.values
 Nacimientos_2021$residuals=model5$residuals
@@ -75,9 +92,12 @@ summary(model5)
 Nacimientos_2021$yhat=model5$fitted.values
 Nacimientos_2021$residuals=model5$residuals
 
+###what is the standard error of variable yhat?
+
 
 ggplot(data=Nacimientos_2021)+
   geom_point(aes(x=yhat, y=residuals), alpha=0.7)
+
 
 #### 
 #What else do you notice?
