400af20c3cbd15d1bda933e99960e6b6f6f7ae43
KrzysztofZaremba <kzysiek92@gmail.com>
2024-08-12 18:39:49 -0600

    slides2update

--- C2/To_pdf.R
+++ C2/To_pdf.R
@@ -1,5 +1,10 @@
+remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
+
+install.packages("remotes")
 library(renderthis)
-#remotes::install_github('rstudio/chromote')
-to_pdf("C_2_slides_d.html")
+remotes::install_github('rstudio/chromote')
+to_pdf("C_2_slides_a.html")
 
 ?to_pdf
+Sys.getenv("GITHUB_PAT")
+Sys.unsetenv("GITHUB_PAT")
