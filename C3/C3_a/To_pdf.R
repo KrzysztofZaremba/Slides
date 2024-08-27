56d51b92525a0eab99b6d9665e52d72b9f6909cd
KrzysztofZaremba <kzysiek92@gmail.com>
2024-08-26 21:06:12 -0600

    commit

--- C3/C3_a/To_pdf.R
+++ C3/C3_a/To_pdf.R
@@ -1,5 +1,8 @@
 library(renderthis)
 #remotes::install_github('rstudio/chromote')
-to_pdf("C_3_slides_a.html")
+to_pdf("C_3_slides_pdf.html")
 
 
+#library(renderthis)
+#to_pdf("C_2_slides_c.html")
+pagedown::chrome_print(input = "C_3_slides_pdf.html", output = "C_3_slides_a.pdf", timeout = 300)
