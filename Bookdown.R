9fd1aae287f789f7ae697a178e538b6c2f0e1689
KrzysztofZaremba <60619861+KrzysztofZaremba@users.noreply.github.com>
2025-05-19 14:37:25 -0600

    update

--- Bookdown.R
+++
@@ -1,3 +0,0 @@
-usethis::use_git_config(user.name = "KrzysztofZaremba", user.email = "kzysiek92@gmail.com")
-usethis::create_github_token() 
-credentials::set_github_pat("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")
