74a4ab4c4169880565cb486b040c7635ec734097
KrzysztofZaremba <60619861+KrzysztofZaremba@users.noreply.github.com>
2025-08-11 19:53:51 -0600

    update

--- C2/To_pdf.R
+++ C2/To_pdf.R
@@ -4,9 +4,8 @@
 #install.packages("renderthis")
 #remotes::install_github('yihui/servr')
 
-library(renderthis)
-to_pdf("C_2_slides_c.html")
-pagedown::chrome_print(input = "C_2_slides_c.html", output = "C_2_slides_c.pdf", timeout = 300)
+
+pagedown::chrome_print(input = "C_2_slides_d.html", output = "C_2_slides_d.pdf", timeout = 300)
 # 
 # usethis::git_sitrep()
 # 
--- C3/C3_a/To_pdf.R
+++ C3/C3_a/To_pdf.R
@@ -5,4 +5,4 @@ to_pdf("C_3_slides_pdf.html")
 
 #library(renderthis)
 #to_pdf("C_2_slides_c.html")
-pagedown::chrome_print(input = "C_3_slides_pdf.html", output = "C_3_slides_a.pdf", timeout = 300)
+pagedown::chrome_print(input = "C_3_slides_a.html", output = "C_3_slides_a.pdf", timeout = 300)
