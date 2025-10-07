f29e1f1453255b5f0eafd9067e8443d0ced9f38e
KrzysztofZaremba <60619861+KrzysztofZaremba@users.noreply.github.com>
2025-10-06 18:45:29 -0600

    update

--- C5/C5_a/To_pdf.R
+++ C5/C5_a/To_pdf.R
@@ -1,8 +1,9 @@
-library(renderthis)
+#library(renderthis)
 
 
 #remotes::install_github("jhelvy/renderthis")
 #remotes::install_github('rstudio/chromote')
 
-to_pdf("C_5_slides_a.html")
+#to_pdf("C_5_slides_a.html")
 
+pagedown::chrome_print(input = "C_5_slides_a.html", output = "C_5_slides_a.pdf", timeout = 300)
