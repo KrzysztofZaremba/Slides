c8942de3fc7d60ef9ecd39923bec33fc7eaae850
KrzysztofZaremba <60619861+KrzysztofZaremba@users.noreply.github.com>
2025-10-13 13:37:11 -0600

    update

--- C4/C4_b/To_pdf.R
+++ C4/C4_b/To_pdf.R
@@ -1,3 +1,2 @@
-library(renderthis)
-remotes::install_github('rstudio/chromote')
-to_pdf("C_4_slides_a.html")
+pagedown::chrome_print(input = "C:/Users/kzysi/Dropbox/Itam_teaching/Markdowns/C4/C4_a/C_4_slides_a.html",
+                       output = "C:/Users/kzysi/Dropbox/Itam_teaching/Markdowns/C4/C4_a/C_4_slides_a.pdf", timeout = 300)
